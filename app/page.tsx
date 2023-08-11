d814d618649736692f0ff4d596446f516745f646
albonwu <albonwu@umich.edu>
2023-08-10 23:56:11 -0500

    shapes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HomeAuth from "@/components/authfield/HomeAuth";
+import BackgroundShapes from "@/components/BackgroundShapes";
+import Image from "next/image";
 
 const Home: React.FC = () => {
   return (
     <>
       <HomeAuth />
+      <BackgroundShapes />
     </>
   );
 };
--- components/BackgroundShapes.tsx
+++ components/BackgroundShapes.tsx
@@ -2,7 +2,29 @@ import React from "react";
 import Image from "next/image";
 
 const BackgroundShapes: React.FC = () => {
-    return <><Image src={"arrowleft.svg"} alt="arrowleft" /></>
-}
+  return (
+    <>
+      <div style={{ position: "relative", left: "10vw", top: "-300px" }}>
+        <Image
+          src={"arrowleft.svg"}
+          alt="arrowleft"
+          width={150}
+          height={150}
+        />
+      </div>
+      <div style={{ position: "relative", left: "65vw", top: "-330px" }}>
+        <Image
+          src={"arrowRight.svg"}
+          alt="arrowright"
+          width={300}
+          height={300}
+        />
+      </div>
+      <div style={{ position: "relative", left: "75vw", top: "-700px" }}>
+        <Image src={"circle.svg"} alt="arrowleft" width={100} height={100} />
+      </div>
+    </>
+  );
+};
 
 export default BackgroundShapes;
